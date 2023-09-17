5e4d77826bdcc16d3aeb4bd8bb8e40a0fe03923f
xongjaemin <woals99@gmail.com>
2023-09-17 23:34:39 +0900

    REFACTOR: 공통 메뉴바 컴포넌트 layout.tsx로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { IconNext, IconVercel } from '../../public/svgs'
 import MenuBar from '@/components/MenuBar'
-import Object from '@/app/object'
 
 export default function Home() {
-  return (
-    <main>
-      {/* 좌측 고정 메뉴바 */}
-      <MenuBar />
-      <Object />
-    </main>
-  )
+  return <main></main>
 }
