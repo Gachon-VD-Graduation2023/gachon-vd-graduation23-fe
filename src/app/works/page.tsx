6f2ac63a0d2330eb498622d005db7cada5ce187a
xongjaemin <woals99@gmail.com>
2023-09-30 00:34:22 +0900

    feat: works 리스트 퍼블리싱

---
+++ src/app/works/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React from 'react'
+import WorkThumbnail from '@/components/Works/WorkThumbnail'
+import SplashThumbnail from '@/components/Works/SplashThumbnail'
+import styled from 'styled-components'
+
+export default function Works() {
+  const data = [
+    { img: '', title: '작품1', artist: '작가1' },
+    { img: '', title: '작품2', artist: '작가2' },
+    { img: '', title: '작품3', artist: '작가3' },
+    { img: '', title: '작품4', artist: '작가4' },
+    { img: '', title: '작품5', artist: '작가5' },
+    { img: '', title: '작품6', artist: '작가6' },
+    { img: '', title: '작품7', artist: '작가7' },
+    { img: '', title: '작품8', artist: '작가8' },
+    { img: '', title: '작품9', artist: '작가9' },
+    { img: '', title: '작품10', artist: '작가10' },
+    { img: '', title: '작품11', artist: '작가11' },
+    { img: '', title: '작품12', artist: '작가12' },
+    { img: '', title: '작품13', artist: '작가13' },
+    { img: '', title: '작품14', artist: '작가14' },
+    { img: '', title: '작품15', artist: '작가15' },
+    { img: '', title: '작품16', artist: '작가16' },
+    { img: '', title: '작품17', artist: '작가17' },
+    { img: '', title: '작품18', artist: '작가18' },
+    { img: '', title: '작품19', artist: '작가19' },
+    { img: '', title: '작품20', artist: '작가20' },
+    { img: '', title: '작품20', artist: '작가21' },
+    { img: '', title: '작품20', artist: '작가22' },
+  ]
+  return (
+    <Container>
+      <Contents>
+        {data.map((data, i) =>
+          i !== 0 && (i + 1) % 7 === 0 ? (
+            <SplashThumbnail idx={i} />
+          ) : (
+            <WorkThumbnail data={data} key={i} />
+          ),
+        )}
+      </Contents>
+    </Container>
+  )
+}
+const Container = styled.div`
+  padding: 62px 0px 62px 201px;
+  background: skyblue;
+  min-height: 100vh;
+`
+const Contents = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(289px, max-content));
+  justify-content: center;
+  grid-row-gap: 10px;
+`
---
+++ src/components/Works/WorkThumbnail.tsx
@@ -0,0 +1,58 @@
+'use client'
+import React from 'react'
+import styled from 'styled-components'
+
+type DataInfo = {
+  img: string
+  title: string
+  artist: string
+}
+
+interface PropsData {
+  data: DataInfo
+}
+
+function WorkThumbnail(props: PropsData) {
+  const { img, title, artist } = props.data
+
+  return (
+    <Container>
+      <ThumbnailImage>
+        <img src='http://www.goldinglass.com/images/Projects/288x288-Matrimandir.jpg' />
+      </ThumbnailImage>
+      <InfoContainer>
+        <Title>{title}</Title>
+        <Artist>{artist}</Artist>
+      </InfoContainer>
+    </Container>
+  )
+}
+
+export default WorkThumbnail
+const Container = styled.div`
+  width: 288px;
+  height: 357px;
+  background: rgba(255, 255, 255, 0.45);
+  backdrop-filter: blur(24px);
+  box-shadow: 0px 0px 1px 0.5px black; /* box-shadow를 사용하여 대체 */
+`
+const ThumbnailImage = styled.div`
+  width: 288px;
+  height: 288px;
+`
+const InfoContainer = styled.div`
+  height: 69px;
+  padding: 16px;
+  margin-top: 0px;
+`
+const Title = styled.div`
+  color: #141414;
+  font-size: 16px;
+  font-weight: 700;
+`
+const Artist = styled.div`
+  color: #6a6a6a;
+  text-align: right;
+  font-size: 14px;
+  font-weight: 700;
+`
