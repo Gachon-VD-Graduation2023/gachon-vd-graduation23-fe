26f97f37a27b7bede57102132c1fcc80d50b28d7
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2023-10-01 23:10:53 +0900

    update firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+import { getDatabase } from 'firebase/database'
+// import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,8 +15,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_REACT_APP_DATABASE_URL,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+export const database = getDatabase(app)
+// const analytics = getAnalytics(app)
