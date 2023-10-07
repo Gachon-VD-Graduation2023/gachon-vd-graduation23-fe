26a1986b1d7003a58ef1d0d046798231349e2aa2
xongjaemin <woals99@gmail.com>
2023-10-08 02:58:04 +0900

    feat: 모바일 브라우저 대응 vh값 get 모듈

--- src/utils/common.util.ts
+++ src/utils/common.util.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
-export default function useBetterMediaQuery(mediaQueryString: string) {
+export function useBetterMediaQuery(mediaQueryString: string) {
   const [matches, setMatches] = useState<boolean>()
 
   useEffect(() => {
@@ -12,4 +12,27 @@ export default function useBetterMediaQuery(mediaQueryString: string) {
   }, [mediaQueryString])
 
   return matches
+}
+
+const getWindowInnerHeight = () => Number((window.innerHeight * 0.01).toFixed(2))
+export function useVh(): number {
+  const [vh, setVh] = useState<number>(0)
+
+  const updateVh = useCallback(() => {
+    const innerHeight = getWindowInnerHeight()
+
+    document.documentElement.style.setProperty('--vh', `${innerHeight}px`)
+    setVh(innerHeight)
+  }, [setVh])
+
+  useEffect(() => {
+    updateVh()
+    window.addEventListener('resize', updateVh)
+
+    return () => {
+      window.removeEventListener('resize', updateVh)
+    }
+  }, [updateVh])
+
+  return vh
 }
