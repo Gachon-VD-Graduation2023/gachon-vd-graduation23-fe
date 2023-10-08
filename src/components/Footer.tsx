2769876c5e2646b399d0ad16e3b5a386fe6fd0fd
xongjaemin <woals99@gmail.com>
2023-10-08 23:59:29 +0900

    feat: 공통 Footer 모바일 ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,38 @@
 'use client'
 import React from 'react'
 import styled from 'styled-components'
+import { useBetterMediaQuery } from '@/utils/common.util'
 
 export default function Footer() {
+  const isMobile = useBetterMediaQuery('(max-width: 500px)')
   const baseUrl = process.env.NEXT_PUBLIC_URL
   return (
-    <Container>
+    <Container mobile={isMobile?.toString()}>
       <LogoImg src={baseUrl + '/images/title-logo.png'} />
-      <Credit>
-        <div className='title'>2023 가천대학교 시각디자인과 졸업전시회</div>
-        <div className='copyright'>{'<대박> Ⓒ 2023 Gachon VCD All rights reserved.'}</div>
-      </Credit>
+      {!isMobile ? (
+        <Credit>
+          <div className='title'>2023 가천대학교 시각디자인과 졸업전시회</div>
+          <div className='copyright'>{'<대박> Ⓒ 2023 Gachon VCD All rights reserved.'}</div>
+        </Credit>
+      ) : (
+        <MobileCredit>
+          {`2023 가천대학교 시각디자인과 졸업전시회 \n<대박> Ⓒ 2023 Gachon VCD All rights reserved.`}
+        </MobileCredit>
+      )}
     </Container>
   )
 }
 
-const Container = styled.div`
+const Container = styled.div<{ mobile?: string }>`
   display: flex;
   justify-content: space-between;
+  position: relative;
   align-items: center;
   border-top: 1px solid black;
   width: 100%;
-  height: 101px;
-  padding-left: 229px;
-  padding-right: 40px;
+  height: ${(props) => (props.mobile === 'true' ? '76px;' : '101px')};
+  padding-left: ${(props) => (props.mobile === 'true' ? '16px;' : '229px')};
+  padding-right: ${(props) => (props.mobile === 'true' ? '16px;' : '40px')};
 `
 const LogoImg = styled.img`
   display: block;
@@ -46,4 +55,16 @@ const Credit = styled.div`
     font-size: 12px;
     font-weight: 700;
   }
+`
+
+const MobileCredit = styled.div`
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+  text-align: right;
+  font-size: 10px;
+  font-weight: 700;
+  line-height: 14px;
+  color: #4d4d4d;
+  white-space: pre-wrap;
 `
